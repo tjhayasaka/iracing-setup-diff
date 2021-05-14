cae7e8da9b85eb04ca2cd49ca3cd07c9683df01b
Tomoaki Hayasaka <hayasaka@malmodeler.com>
2021-05-15 07:42:16 +0900

    Master:  fix track and car names.

--- src/Master.elm
+++ src/Master.elm
@@ -17,11 +17,11 @@ toDict items =
 cars : Dict Car.Id Car.Car
 cars =
     toDict
-        [ { id = 80, shortName = "dirtsprint winged 305", longName = "Dirt Sprint Car - 305" }
-        , { id = 85, shortName = "dirtsprint winged 360", longName = "Dirt Sprint Car - 360" }
-        , { id = 86, shortName = "dirtsprint winged 410", longName = "Dirt Sprint Car - 410" }
-        , { id = 87, shortName = "dirtsprint nonwinged 360", longName = "Dirt Sprint Car - 360 Non-Winged" }
-        , { id = 89, shortName = "dirtsprint nonwinged 410", longName = "Dirt Sprint Car - 410 Non-Winged" }
+        [ { id = 80, shortName = "dirtsprint/winged 305", longName = "Dirt Sprint Car - 305" }
+        , { id = 85, shortName = "dirtsprint/winged 360", longName = "Dirt Sprint Car - 360" }
+        , { id = 86, shortName = "dirtsprint/winged 410", longName = "Dirt Sprint Car - 410" }
+        , { id = 87, shortName = "dirtsprint/nonwinged 360", longName = "Dirt Sprint Car - 360 Non-Winged" }
+        , { id = 89, shortName = "dirtsprint/nonwinged 410", longName = "Dirt Sprint Car - 410 Non-Winged" }
         , { id = 96, shortName = "dirtmidget", longName = "Dirt Midget" }
         ]
 
@@ -30,11 +30,11 @@ tracks : Dict Track.Id Track.Track
 tracks =
     toDict
         [ { id = -1, shortName = "baseline", longName = "<baseline>" }
-        , { id = 275, shortName = "lakeland dirt", longName = "USA International Speedway" }
+        , { id = 275, shortName = "lakeland_dirt", longName = "USA International Speedway" }
         , { id = 273, shortName = "eldora", longName = "Eldora Speedway" }
         , { id = 274, shortName = "williamsgrove", longName = "Williams Grove Speedway" }
         , { id = 279, shortName = "volusia", longName = "Volusia Speedway Park" }
-        , { id = 287, shortName = "bristol dirt", longName = "Bristol Motor Speedway - Dirt" }
+        , { id = 287, shortName = "bristol_dirt", longName = "Bristol Motor Speedway - Dirt" }
         , { id = 288, shortName = "lanier_dirt", longName = "Lanier National Speedway - Dirt" }
         , { id = 303, shortName = "limaland", longName = "Limaland Motorsports Park" }
         , { id = 305, shortName = "knoxville", longName = "Knoxville Raceway" }
